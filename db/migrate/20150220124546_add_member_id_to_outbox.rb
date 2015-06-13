e90a560098e15793e28fec7c142e07cc1d4e8755
pratap224 <pratap224@gmail.com>
2015-06-13 17:58:18 +0530

    out

--- db/migrate/20150220124546_add_member_id_to_outbox.rb
+++
@@ -1,5 +0,0 @@
-class AddMemberIdToOutbox < ActiveRecord::Migration
-  def change
-    add_column :outboxes, :member_id, :integer
-  end
-end
--- db/migrate/20150221041951_add_from_to_outboxes.rb
+++
@@ -1,5 +0,0 @@
-class AddFromToOutboxes < ActiveRecord::Migration
-  def change
-    add_column :outboxes, :from, :string
-  end
-end
